f773b8945fb75587ad8f98dc5c0a103b10aaa1f0
playstation4 <teppopp@mail.ru>
2016-11-05 16:13:10 +0500

    Create adapter

---
+++ Adapter/Adapter/Adapter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Example_04.Homework.FirstOrmLibrary;
+using Example_04.Homework.Models;
+using Example_04.Homework.Models.Interfaces;
+using Example_04.Homework.SecondOrmLibrary;
+
+namespace Adapter
+{
+    public class Adapter<TDbEntity> : IFirstOrm<TDbEntity> where TDbEntity : IDbEntity
+    {
+        private ISecondOrm secondOrm;
+
+        public Adapter(ISecondOrm secondOrm)
+        {
+            this.secondOrm = secondOrm;
+        }
+
+        public void Create(TDbEntity entity)
+        {
+            if (entity is DbUserEntity)
+            {
+                secondOrm.Context.Users.Add(entity as DbUserEntity);
+            }
+            else if (entity is DbUserInfoEntity)
+            {
+                secondOrm.Context.UserInfos.Add(entity as DbUserInfoEntity);
+            }
+            else throw new ArgumentException();
+        }
+
+        public TDbEntity Read(int id)
+        {
+            return (TDbEntity)secondOrm.Context.Users.Concat<IDbEntity>(secondOrm.Context.UserInfos)
+                .FirstOrDefault(e => e.Id == id);
+        }
+
+        public void Update(TDbEntity entity)
+        {
+            Delete(entity);
+            Create(entity);
+        }
+
+        public void Delete(TDbEntity entity)
+        {
+            if (entity is DbUserEntity)
+            {
+                secondOrm.Context.Users.Remove(entity as DbUserEntity);
+            }
+            else if (entity is DbUserInfoEntity)
+            {
+                secondOrm.Context.UserInfos.Remove(entity as DbUserInfoEntity);
+            }
+            else throw new ArgumentException();
+        }
+    }
+}
---
+++ Adapter/Adapter/UseExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Example_04.Homework.FirstOrmLibrary;
+using Example_04.Homework.Models;
+using Example_04.Homework.Models.Interfaces;
+using Example_04.Homework.SecondOrmLibrary;
+
+namespace Adapter
+{
+    class UseExample
+    {
+        public static void Main(string[] args)
+        {
+            ISecondOrm second = null;
+            IFirstOrm<IDbEntity> first = new Adapter<IDbEntity>(second);
+
+            var entityUser = new DbUserEntity();
+            var entityUserInfo = new DbUserInfoEntity();
+
+            first.Create(entityUser);
+            first.Create(entityUserInfo);
+        }
+    }
+}
